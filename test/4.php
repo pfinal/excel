0c49c06ee6733b32cc0cc6dd1d82870a8e0600e6
Ethan <nicesoft333@qq.com>
2019-05-13 13:31:22 +0800

    dev

---
+++ test/4.php
@@ -0,0 +1,30 @@
+<?php
+
+include '../vendor/autoload.php';
+
+use PFinal\Excel\Excel;
+
+date_default_timezone_set('PRC');
+
+$map = array(
+    'title' => array(
+        'id' => '编号',
+        'name' => '姓名',
+        'age' => '年龄',
+    )
+);
+
+
+Excel::chunkExportCSV($map, './temp.csv', function ($writer) {
+
+    $data = array(
+        array('id' => 1, 'name' => 'Jack', 'age' => 18),
+        array('created_at' => '2019-02-02', 'name' => 'Mary', 'id' => 2, 'age' => 20,),
+        array('id' => 3, 'name' => '"Ethan', 'age' => 34),
+        array('id' => 4, 'name' => '\'Tony', 'age' => 34),
+        array('id' => 5, 'name' => ',', 'age' => 34),
+    );
+
+    /**  \Closure $writer */
+    $writer($data);
+});
